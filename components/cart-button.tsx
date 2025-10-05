c7ef9762f84a285e9fa4345f3766a2d9e1f05331
p.andhika <p.andhika@op.iitg.ac.in>
2025-10-05 18:37:43 +0700

    feat(cart): add styled cart button with item count display

--- components/cart-button.tsx
+++ components/cart-button.tsx
@@ -1,14 +1,42 @@
+import { COLORS } from "@/utils/colors";
 import { useCartStore } from "@/utils/store/cart";
-import { View, Text } from "react-native";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 
 const CartButton = () => {
   const { count } = useCartStore();
 
   return (
-    <View>
-      <Text>{count}</Text>
-    </View>
+    <TouchableOpacity>
+      {count > 0 && (
+        <View style={styles.container}>
+          <Text style={styles.text}>{count}</Text>
+        </View>
+      )}
+
+      <Ionicons name="cart" size={28} />
+    </TouchableOpacity>
   );
 };
 
 export default CartButton;
+
+const styles = StyleSheet.create({
+  container: {
+    position: "absolute",
+    right: -10,
+    bottom: -5,
+    backgroundColor: COLORS.primary,
+    borderRadius: 10,
+    width: 20,
+    height: 20,
+    justifyContent: "center",
+    alignItems: "center",
+    zIndex: 1,
+  },
+  text: {
+    fontSize: 12,
+    color: "white",
+    fontWeight: "bold",
+  },
+});
